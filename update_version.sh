a22dee71e5b6b97bc17f31d9b0ef8163d86ba19d
Jonas Pleyer <jonas.dev@pleyer.org>
2025-03-05 08:31:51 +0100

    bump version to 0.5.2
    - bugfix: institutes-size

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,5 @@
-OLD_VERSION=0\.5\.0
-NEW_VERSION=0\.5\.1
+OLD_VERSION=0\.5\.1
+NEW_VERSION=0\.5\.2
 
 for file in $(grep -lr $OLD_VERSION --exclude-dir docs .); do
     sed -i "s/$OLD_VERSION/$NEW_VERSION/" $file
