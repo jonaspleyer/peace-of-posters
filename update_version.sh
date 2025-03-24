060f65ad90827a7a1e5c4873eb0db040f5e6150b
Jonas Pleyer <jonas.dev@pleyer.org>
2025-03-24 13:05:54 +0100

    bump to 0.5.4
    - includes bugfix for stretch-to-next

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,5 @@
-OLD_VERSION=0\.5\.2
-NEW_VERSION=0\.5\.3
+OLD_VERSION=0\.5\.3
+NEW_VERSION=0\.5\.4
 
 for file in $(grep -lr $OLD_VERSION --exclude-dir docs .); do
     sed -i "s/$OLD_VERSION/$NEW_VERSION/" $file
