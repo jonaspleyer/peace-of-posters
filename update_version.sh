64b6ca02409efa2b67f8d207e1d6e0656bd223d1
Jonas Pleyer <jonas.dev@pleyer.org>
2025-04-09 18:48:20 +0200

    bump to 0.5.5

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,5 @@
-OLD_VERSION=0\.5\.3
-NEW_VERSION=0\.5\.4
+OLD_VERSION=0\.5\.4
+NEW_VERSION=0\.5\.5
 
 for file in $(grep -lr $OLD_VERSION --exclude-dir docs .); do
     sed -i "s/$OLD_VERSION/$NEW_VERSION/" $file
