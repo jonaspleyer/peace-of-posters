d046123aa9822ff64703782821b3f2cb1e4f973d
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-20 17:37:00 +0200

    exclude git directory

--- update_version.sh
+++ update_version.sh
@@ -1,6 +1,6 @@
 OLD_VERSION=0\.5\.4
 NEW_VERSION=0\.5\.5
 
-for file in $(grep -lr $OLD_VERSION --exclude-dir docs .); do
+for file in $(grep -lr $OLD_VERSION --exclude-dir docs --exclude-dir .git .); do
     sed -i "s/$OLD_VERSION/$NEW_VERSION/" $file
 done
